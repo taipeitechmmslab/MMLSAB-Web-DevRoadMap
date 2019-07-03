c3351b39adca55bd2f19a023a7c9ea1a3d2fa2e9
thiefjim <thiefjim@gmail.com>
2019-07-03 17:44:11 +0800

    update no gulp s1

--- no-gulp-step1-init/routes/backstage.js
+++ no-gulp-step1-init/routes/backstage.js
@@ -1,3 +1,7 @@
+exports.root = (req, res) => {
+    res.status(200).send("root"); //回應HTTP StatusCode 「200」，HTTP body的文字為「root」
+};
+
 exports.index = (req, res) => {
-    res.status(200).send("OK!!!!");
+    res.status(200).send("index"); //回應HTTP StatusCode 「200」，HTTP body的文字為「index」
 };
