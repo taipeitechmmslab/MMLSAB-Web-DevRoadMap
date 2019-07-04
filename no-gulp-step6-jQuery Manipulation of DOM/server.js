f12ba8af5a4af5e0edd8d6aa9a75b5c56a887578
thiefjim <thiefjim@gmail.com>
2019-07-04 16:35:24 +0800

    lab6

--- no-gulp-step6-jQuery Manipulation of DOM/server.js
+++ no-gulp-step6-jQuery Manipulation of DOM/server.js
@@ -1,15 +1,13 @@
 // import libraries
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
-var request = require('request');
 
 // initial express
 var app = express();
 var port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0');
+app.listen(port, '0.0.0.0', ()=> console.log(`Running on ${port}`)); //印出監聽的port
 
-app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, 'app'))); //掛載靜態檔案app資料夾的內容
 
-require('./routes/index.js')(app);
+require('./routes/index.js')(app); //路由控制
 module.exports = app;
