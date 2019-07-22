fd03323b462d464fa66ec4f3de30acd7da1bc31c
thiefjim <thiefjim@gmail.com>
2019-07-22 15:39:32 +0800

    react lab3

---
+++ react-step3-axios-get-data/src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // 設定 state
+    this.state = {
+      userList: []
+    };
+  }
+  componentDidMount() {
+    //指定網址，並執行axios的GET方法
+    axios.get("https://jsonplaceholder.typicode.com/users").then(response => {
+      this.setState({userList:response.data})
+    });
+  }
+  render() {
+    // 從 state 取出資料
+    let userList = this.state.userList;
+
+    return (
+      <div>
+        <h1 id="title">使用者查詢example1</h1>
+        <table id="personList" class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">name</th>
+              <th scope="col">phone</th>
+              <th scope="col">email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userList.map(user => {
+              // 傳回 jsx
+              return (
+                <tr key={user.id}>
+                  <th scope="row">{user.id}</th>
+                  <td>{user.name}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.email}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
