e302ef7b60066c18687ebca9bf712f01f8457f3b
thiefjim <thiefjim@gmail.com>
2019-07-23 10:07:27 +0800

    optimize react lab5

--- react-step5-react-router/src/index.js
+++ react-step5-react-router/src/index.js
@@ -4,30 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import UserInfo from "./user/userInfo";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-class RouterHome extends React.Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/users">All Users</Link>
-          </li>
-          <li>
-            <Link to="/users/1">Users 1</Link>
-          </li>
-          <li>
-            <Link to="/users/2">Users 2</Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+import RouterHome from './route/routeHome';
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
